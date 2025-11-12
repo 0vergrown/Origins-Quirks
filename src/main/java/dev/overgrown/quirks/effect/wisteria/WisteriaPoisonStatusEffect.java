51e07c3918331b96207f7118fcace32c58c62595
Overgrown <overgrownmc@gmail.com>
2025-11-11 21:49:14 -0500

    Modified Wisteria Poison Effect to Slow Entities

--- src/main/java/dev/overgrown/quirks/effect/wisteria/WisteriaPoisonStatusEffect.java
+++ src/main/java/dev/overgrown/quirks/effect/wisteria/WisteriaPoisonStatusEffect.java
@@ -2,14 +2,29 @@ package dev.overgrown.quirks.effect.wisteria;
 
 import dev.overgrown.quirks.Quirks;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 
+import java.util.UUID;
+
 public class WisteriaPoisonStatusEffect extends StatusEffect {
+    // Custom UUID for the movement speed modifier
+    private static final UUID MOVEMENT_SPEED_MODIFIER_ID = UUID.fromString("7107DE5E-7CE8-4030-940E-514C1F160891");
+
     public WisteriaPoisonStatusEffect() {
         super(StatusEffectCategory.HARMFUL, 0x8A2BE2); // Purple color
+
+        // Add attribute modifier for movement speed reduction
+        this.addAttributeModifier(
+                EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                MOVEMENT_SPEED_MODIFIER_ID.toString(),
+                -0.20, // 20% reduction per level
+                EntityAttributeModifier.Operation.MULTIPLY_TOTAL
+        );
     }
 
     @Override
